ed013386f8d09385618f2ecd4d8570f88dee10e2
Arktoss <adamb2106@gmail.com>
2023-03-16 10:49:12 -0400

    f

--- 1_Getting_Started_with_MySQL/part_5.1.py
+++ 1_Getting_Started_with_MySQL/part_5.1.py
@@ -14,14 +14,14 @@ sqlFormula = "INSERT INTO turtles (name, color, weapon, pizza, age) VALUES (%s, 
 #leonardo = ("Leonardo", "Blue", "Katana", False, 15)
 turtles = [("Raphael", "Red", "Sai", False, 19),
            ("Donatello", "Purple", "Bo", False, 17),
-           ("Michelangelo", "Orange", "Nunchucks", False, 18)
+           ("Michelangelo", "Orange", "Nunchucks", False, 18),
            ("Bob", "Green", "Si", False, 19),
            ("Michelle", "Nurple", "Ko", False, 17),
            ("Michele", "Brown", "Nunnery", False, 19),
            ("Tommy two tone", "Green", "Scion", False, 19),
            ("Jenni", "Nurple", "Low Staff", False, 17),
            ("Sho Nuff", "Brown", "Chuck Norris", False, 8),
-           ("Bruce Lee Roy Green", "Brown", "Spong bob", False, 29),]
+           ("Bruce Lee Roy Green", "Brown", "Spong bob", False, 29)]
 
 #mycursor.execute(sqlFormula, leonardo)
 mycursor.executemany(sqlFormula, turtles)
